b47b37c397a246d261ba499850ec55139ef30859
Stefan Scherer <scherer_stefan@icloud.com>
2018-10-04 15:55:56 +0200

    Cleanup more images

--- 4-portainer/housekeeping/cleanup.ps1
+++ 4-portainer/housekeeping/cleanup.ps1
@@ -7,4 +7,8 @@ docker rmi chocolateyfest/node:latest
 docker rmi chocolateyfest/node:pure
 docker rmi chocolateyfest/node:runtime
 docker rmi chocolateyfest/portainer:latest
+docker rmi chocolateyfest/portainer:1.19.2
+docker rmi chocolateyfest/node:10.11.0-runtime
+docker rmi chocolateyfest/node:10.11.0
+docker rmi chocolateyfest/chocolatey:0.10.11
 docker system prune -f
